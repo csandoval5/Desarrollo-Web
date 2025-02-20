7250c991240bc7867c639246f16fb169f7c71cca
csandoval5 <cf.sandovalc@uea.edu.ec>
2025-02-20 10:35:55 -0500

    Actualizacion para la tarea de la semana 10

--- mi_proyecto_flask/app.py
+++ mi_proyecto_flask/app.py
@@ -1,13 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Hola mundo, esta es mi tarea de la semana 9"
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 @app.route('/Cristian')
 def usuario():
-    return 'Bienvenido, Cristian Sandoval!'
+    return "Bienvenido, Cristian Sandoval!"
 
 if __name__ == '__main__':
     app.run(debug=True)
